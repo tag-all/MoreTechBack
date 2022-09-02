c425ca095c4c09f2bd5e0280c587e5a819967ae5
Iurii Babalin <babalin2011@gmail.com>
2022-09-02 18:27:42 +0300

    init

---
+++ src/main/java/ru/tagallteam/hackstarter/HackStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.tagallteam.hackstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tagallteam/hackstarter/HackStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tagallteam.hackstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
