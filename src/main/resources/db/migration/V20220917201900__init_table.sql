c8bf32eb5955afe992353250071f934ed251fe06
Iuri Babalin <babalin2011@gmail.com>
2022-10-07 01:15:27 +0300

    some models

--- src/main/resources/db/migration/V20220917201900__init_table.sql
+++ src/main/resources/db/migration/V20220917201900__init_table.sql
@@ -28,10 +28,10 @@ create sequence activity_seq;
 
 create table activity
 (
-    id                 int         not null
+    id               int         not null
         constraint pk_activities primary key,
-    user_id            int references customer (id),
-    type               varchar(64) not null,
+    user_id          int references customer (id),
+    type             varchar(64) not null,
     activity_link_id int,
     activity_date    timestamp   not null
 );
@@ -40,11 +40,11 @@ create sequence file_seq;
 
 create table file
 (
-    id   uuid        not null
+    id          uuid      not null
         constraint pk_file primary key,
-    name text not null
-    expansion varchar(10) not null,
-    data text not null,
+    name        text      not null
+        expansion varchar (10) not null,
+    data        text      not null,
     create_date timestamp not null
 );
 
@@ -52,13 +52,42 @@ create sequence achievement_seq;
 
 create table achievement
 (
-    id   int         not null
+    id        int not null
         constraint pk_achievement primary key,
-    file_uuid uuid references file (id)
-    name varchar(64) not null
+    file_uuid uuid references file (id),
+    name varchar (64) not null
+);
+
+create sequence achievement_attribute_seq;
+
+create table achievement_attribute
+(
+    id        int not null
+        constraint pk_achievement_attribute primary key,
+    achievement_id int references achievement (id),
+    type  varchar(32) not null,
+    value text        not null
 );
 
 create table achievement_user
 (
+    user_id        int references customer (id),
+    achievement_id int references achievement (id),
+    achievement_date timestamp not null,
+    primary key (user_id, achievement_id)
+);
 
+
+create sequence product_seq;
+
+create table product
+(
+    id        int not null
+        constraint pk_product primary key,
+    file_uuid uuid references file (id),
+    name varchar (64) not null,
+    price int not null,
+    purchased int not null  default 0,
+    number_of_products int not null default 0,
+    type varchar(64) not null
 );
