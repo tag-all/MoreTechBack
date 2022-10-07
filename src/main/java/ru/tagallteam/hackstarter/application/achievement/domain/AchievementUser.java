b78089251029f7d38c70dc904f2ec6bd684e05da
EgorMIt <egor.mitrofanov@hotmail.com>
2022-10-06 17:08:40 -0700

    achievement

--- src/main/java/ru/tagallteam/hackstarter/application/achievement/domain/AchievementUser.java
+++ src/main/java/ru/tagallteam/hackstarter/application/achievement/domain/AchievementUser.java
@@ -4,25 +4,18 @@ import lombok.Data;
 import ru.tagallteam.hackstarter.application.user.domain.User;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Embeddable
 @Data
 @Entity
 @Table(name = "achievement_user")
 public class AchievementUser {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
-    @SequenceGenerator(name = "sequence", allocationSize = 1, sequenceName = "customer_seq")
-    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
