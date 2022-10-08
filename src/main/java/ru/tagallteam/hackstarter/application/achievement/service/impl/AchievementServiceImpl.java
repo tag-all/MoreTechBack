c004714549bafa3cfcae19a54d778e475f2d8691
EgorMIt <egor.mitrofanov@hotmail.com>
2022-10-08 15:53:52 -0700

    ADD Achievement

--- src/main/java/ru/tagallteam/hackstarter/application/achievement/service/impl/AchievementServiceImpl.java
+++ src/main/java/ru/tagallteam/hackstarter/application/achievement/service/impl/AchievementServiceImpl.java
@@ -11,6 +11,7 @@ import ru.tagallteam.hackstarter.application.achievement.domain.AchievementAttri
 import ru.tagallteam.hackstarter.application.achievement.domain.AchievementAttributeRepository;
 import ru.tagallteam.hackstarter.application.achievement.domain.AchievementRepository;
 import ru.tagallteam.hackstarter.application.achievement.domain.AchievementUser;
+import ru.tagallteam.hackstarter.application.achievement.domain.AchievementUserPk;
 import ru.tagallteam.hackstarter.application.achievement.domain.AchievementUserRepository;
 import ru.tagallteam.hackstarter.application.achievement.mapper.AchievementMapper;
 import ru.tagallteam.hackstarter.application.achievement.model.AchievementDto;
@@ -18,9 +19,14 @@ import ru.tagallteam.hackstarter.application.achievement.service.AchievementServ
 import ru.tagallteam.hackstarter.application.event.modal.EventAttributeType;
 import ru.tagallteam.hackstarter.application.user.domain.User;
 import ru.tagallteam.hackstarter.application.user.domain.UserRepository;
+import ru.tagallteam.hackstarter.application.user.service.UserService;
 import ru.tagallteam.hackstarter.errors.ErrorDescriptor;
+import ru.tagallteam.hackstarter.integration.modal.SendCurrency;
+import ru.tagallteam.hackstarter.integration.service.VtbIntegration;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -31,6 +37,8 @@ import java.util.stream.Collectors;
 public class AchievementServiceImpl implements AchievementService {
 
     private final AchievementRepository achievementRepository;
+    private final UserService userService;
+    private final VtbIntegration vtbIntegration;
     private final AchievementUserRepository achievementUserRepository;
     private final AchievementAttributeRepository achievementAttributeRepository;
     private final UserRepository userRepository;
@@ -83,22 +91,38 @@ public class AchievementServiceImpl implements AchievementService {
         achievementUser.setAchievement(achievement);
         achievementUser.setUser(user);
         achievementUser.setAchievementDate(LocalDateTime.now());
-        achievementUserRepository.save(achievementUser);
+
+        AchievementUserPk achievementUserPk = new AchievementUserPk();
+        achievementUserPk.setAchievementId(achievement.getId());
+        achievementUserPk.setUserId(user.getId());
+        achievementUser.setAchievementUserPk(achievementUserPk);
+        List<AchievementUser> userAchieve = new ArrayList<>(!CollectionUtils.isEmpty(user.getAchievements())
+                ? user.getAchievements()
+                : Collections.emptyList());
+
+        userAchieve.add(achievementUser);
+        user.setAchievements(userAchieve);
+        userRepository.save(user);
+
 
         List<AchievementAttribute> achievementAttributes = achievementAttributeRepository.findAchievementAttributesByAchievement(achievement);
-
         String type = "";
-        long number;
-        for(AchievementAttribute attribute: achievementAttributes){
-            if(Objects.equals(attribute.getType().getName(), EventAttributeType.NUMBER_AWARDS.getName())){
+        long number = 0;
+        for (AchievementAttribute attribute : achievementAttributes) {
+            if (Objects.equals(attribute.getType().getName(), EventAttributeType.NUMBER_AWARDS.getName())) {
                 number = Long.parseLong(attribute.getValue());
-            }
-            else if(Objects.equals(attribute.getType().getName(), EventAttributeType.REWARD_TYPE.getName())){
+            } else if (Objects.equals(attribute.getType().getName(), EventAttributeType.REWARD_TYPE.getName())) {
                 type = attribute.getValue();
             }
         }
-        if(type.equals("XP")){
-
+        if (type.equals("XP")) {
+            userService.addXPtoUser(userId, number);
+        } else if (type.equals("DR")) {
+            vtbIntegration.sendRuble(SendCurrency.builder()
+                    .amount((float) number)
+                    .fromPrivateKey(privateKey)
+                    .toPublicKey(user.getPublicKey())
+                    .build());
         }
     }
 
--- src/main/java/ru/tagallteam/hackstarter/application/lvl/domain/Lvl.java
+++ src/main/java/ru/tagallteam/hackstarter/application/lvl/domain/Lvl.java
@@ -1,13 +1,23 @@
 package ru.tagallteam.hackstarter.application.lvl.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import ru.tagallteam.hackstarter.application.achievement.domain.Achievement;
 import ru.tagallteam.hackstarter.application.user.domain.User;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "Lvl")
 public class Lvl {
