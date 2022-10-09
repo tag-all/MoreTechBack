c530d342eeb78953b1ca5d531087478e1eae2153
Iuri Babalin <babalin2011@gmail.com>
2022-10-09 08:28:54 +0300

    Project

--- src/main/java/ru/tagallteam/hackstarter/MoreTechBackApplication.java
+++ src/main/java/ru/tagallteam/hackstarter/MoreTechBackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HackStarterApplication {
+public class MoreTechBackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HackStarterApplication.class, args);
+        SpringApplication.run(MoreTechBackApplication.class, args);
     }
 
 }
